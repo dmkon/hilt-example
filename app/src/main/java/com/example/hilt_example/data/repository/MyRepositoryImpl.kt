42ce137820740ff8b46d4eeb56775c53ea09a9b0
dmkon <dkondramachine@league.com>
2023-03-20 16:49:07 -0400

    AppModule created.

--- app/src/main/java/com/example/hilt_example/data/repository/MyRepositoryImpl.kt
+++ app/src/main/java/com/example/hilt_example/data/repository/MyRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.example.hilt_example.data.repository
 
+import com.example.hilt_example.data.remote.MyApi
 import com.example.hilt_example.domain.repository.MyRepository
 
-class MyRepositoryImpl: MyRepository {
+class MyRepositoryImpl(
+    private val api: MyApi
+): MyRepository {
     override suspend fun doNetworkCall() {
-        TODO("Not yet implemented")
+
     }
 }
