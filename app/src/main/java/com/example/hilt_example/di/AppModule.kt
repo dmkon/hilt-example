e8bbd2abd87eb242218929d62f1f6627e0510472
dmkon <dkondramachine@league.com>
2023-03-20 17:17:56 -0400

    Two dependencies of same type.

--- app/src/main/java/com/example/hilt_example/di/AppModule.kt
+++ app/src/main/java/com/example/hilt_example/di/AppModule.kt
@@ -9,6 +9,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -26,7 +27,21 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideMyRepository(api: MyApi, app: Application): MyRepository {
+    fun provideMyRepository(
+        api: MyApi,
+        app: Application,
+        @Named("hello1") hello1: String
+    ): MyRepository {
         return MyRepositoryImpl(api, app)
     }
+
+    @Provides
+    @Singleton
+    @Named("hello1")
+    fun providesString1() = "Hello 1"
+
+    @Provides
+    @Singleton
+    @Named("hello2")
+    fun providesString2() = "Hello 2"
 }
